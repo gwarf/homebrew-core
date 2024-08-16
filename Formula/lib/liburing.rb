de3a99a401bfc66f236a37de2b624d6969041858
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-08-16 21:40:18 +0000

    liburing 2.7
    liburing: update test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/liburing.rb
+++ Formula/lib/liburing.rb
@@ -2,8 +2,8 @@ class Liburing < Formula
   desc "Helpers to setup and teardown io_uring instances"
   homepage "https://github.com/axboe/liburing"
   # not need to check github releases, as tags are sufficient, see https://github.com/axboe/liburing/issues/1008
-  url "https://github.com/axboe/liburing/archive/refs/tags/liburing-2.6.tar.gz"
-  sha256 "682f06733e6db6402c1f904cbbe12b94942a49effc872c9e01db3d7b180917cc"
+  url "https://github.com/axboe/liburing/archive/refs/tags/liburing-2.7.tar.gz"
+  sha256 "56202ad443c50e684dde3692819be3b91bbe003e1c14bf5abfa11973669978c1"
   license any_of: ["MIT", "LGPL-2.1-only"]
   head "https://github.com/axboe/liburing.git", branch: "master"
 
@@ -11,6 +11,8 @@ class Liburing < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "a369928abdce516f5cbaedc983900a24b3e22d345f5942e0d62f464578095fbc"
   end
 
+  depends_on "gcc" => :test
+  depends_on "pkg-config" => :test
   depends_on :linux
 
   def install
@@ -30,7 +32,11 @@ class Liburing < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-L#{opt_lib}", "-luring", "-o", "test"
+
+    pkg_config_flags = shell_output("pkg-config --cflags --libs liburing").chomp.split
+    gcc_major_ver = Formula["gcc"].any_installed_version.major
+    gcc = Formula["gcc"].opt_bin/"gcc-#{gcc_major_ver}"
+    system gcc.to_s, "test.c", "-o", "test", *pkg_config_flags
     system "./test"
   end
 end
